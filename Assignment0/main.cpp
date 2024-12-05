29bca1cfa0adc278bd8fab99829ab91cee542271
v_luozhengyu <v_luozhengyu@baidu.com>
2024-12-05 15:37:03 +0800

    作业0

--- Assignment0/main.cpp
+++ Assignment0/main.cpp
@@ -57,6 +57,16 @@ int main(){
     Eigen::Vector3f bv(4.0f, 5.0f, 6.0f);
     Eigen::Vector3f cross_product = av.cross(bv);
     std::cout << "Cross product of v and w: \n" << cross_product << std::endl;
-    
+    // 给定一个点 P =(2,1), 将该点绕原点先逆时针旋转 45◦ ，再平移 (1,2), 计算出变换后点的坐标（要求用齐次坐标进行计算）。
+    Eigen::Vector3f P(2.0f, 1.0f, 1.0f);
+    float angle = 45.0f * M_PI / 180.0f; // 将角度转化为弧度
+    Eigen::Matrix3f rotation_matrix;
+    rotation_matrix << std::cos(angle), -std::sin(angle), 0.0f,
+                       std::sin(angle),  std::cos(angle), 0.0f,
+                       0.0f,            0.0f,            1.0f;
+    Eigen::Vector3f rotated_P = rotation_matrix * P; // 先旋转
+    Eigen::Vector3f translation(1.0f, 2.0f, 0.0f); // 平移向量
+    Eigen::Vector3f transformed_P = rotated_P + translation; // 执行平移
+    std::cout << "Transformed point: \n" << transformed_P << std::endl;
     return 0;
 }   
